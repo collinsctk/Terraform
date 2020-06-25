beb05526db923b6e743574d32a41ce693c492557
collinsctk@qytang.com <collinsctk@qytang.com>
2020-06-25 18:44:52 +0800

    ec2 1 test

--- main.tf
+++ main.tf
@@ -14,27 +14,3 @@ resource "aws_instance" "amazon_linux_2" {
     Name = "qytang ec2"
   }
 }
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_ssh"
-  description = "Allow SSH"
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_all"
-  }
-}
