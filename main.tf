f860e3fbb2f142c6fe6fb0c45b0b4c7973a19325
collinsctk@qytang.com <collinsctk@qytang.com>
2020-06-25 18:41:57 +0800

    ec2 1 test

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "amazon linux 2" {
+resource "aws_instance" "amazon_linux_2" {
   key_name      = "new-aws"
   ami           = "ami-01af223aa7f274198"
   instance_type = "t2.micro"
