82d4fb9047cdc90ded01e84bfb5df7f80ba87f30
collinsctk@qytang.com <collinsctk@qytang.com>
2020-06-25 18:32:53 +0800

    ec2 1 test

--- main.tf
+++ main.tf
@@ -4,25 +4,54 @@ provider "aws" {
   region = var.aws_region
 }
 
-provider "random" {
-  version = "2.2"
+resource "aws_key_pair" "ubuntu" {
+  key_name   = "key"
+  public_key = file("key.pub")
 }
 
-resource "random_pet" "table_name" {}
+resource "aws_instance" "ubuntu" {
+  key_name      = aws_key_pair.ubuntu.key_name
+  ami           = "ami-03ba3948f6c37a4b0"
+  instance_type = "t2.micro"
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
+  tags = {
+    Name = "ubuntu"
+  }
 
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
+  connection {
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("key")
+    host        = self.public_ip
+  }
 
-  attribute {
-    name = "UUID"
-    type = "S"
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_ssh"
+  description = "Allow SSH"
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    user_name = var.tag_user_name
+    Name = "allow_all"
   }
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.ubuntu.id
 }
