57c3eb7023643a9d886abdfd86151078684ebf26
collinsctk@qytang.com <collinsctk@qytang.com>
2020-06-25 18:57:19 +0800

    ec2 1 test

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "amazon_linux_2" {
   key_name      = "new-aws"
   ami           = "ami-01af223aa7f274198"
   instance_type = "t2.micro"
-
+  subnet_id = "${var.default_subnet}"
   tags = {
     Name = "qytang ec2"
   }
