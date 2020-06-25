49f408c8d086fc768bb38344b9c44ed318a28674
collinsctk@qytang.com <collinsctk@qytang.com>
2020-06-25 18:43:10 +0800

    ec2 1 test

--- variables.tf
+++ variables.tf
@@ -2,23 +2,3 @@ variable "aws_region" {
   type    = string
   default = "ap-northeast-2"
 }
-
-variable "db_table_name" {
-  type    = string
-  default = "terraform-learn"
-}
-
-variable "db_read_capacity" {
-  type    = number
-  default = 5
-}
-
-variable "db_write_capacity" {
-  type    = number
-  default = 5
-}
-
-variable "tag_user_name" {
-  type = string
-  default = "db_tag"
-}
